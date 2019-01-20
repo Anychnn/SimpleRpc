8a648693f1fe82888f941dec2620fcae9addbbbd
anyang <496812133@qq.com>
2019-01-21 00:09:47 +0800

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Anyang on 2019/1/20.
+ */
+public class Main {
+}
---
+++ src/main/java/annotate/Service.java
@@ -0,0 +1,7 @@
+package annotate;
+
+/**
+ * Created by Anyang on 2019/1/21.
+ */
+public @interface Service {
+}
