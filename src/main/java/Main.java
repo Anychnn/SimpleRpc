b6225c62db034240572c5eb2d858eee3ecfdf8a3
anyang <496812133@qq.com>
2019-01-29 02:06:59 +0800

    init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 /**
  * Created by Anyang on 2019/1/20.
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Bootstrap bootstrap=new Bootstrap();
+        EventLoopGroup eventLoopGroup=new NioEventLoopGroup();
+        bootstrap.group(eventLoopGroup)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.SO_KEEPALIVE,true)
+                .option(ChannelOption.TCP_NODELAY,true)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS,6000)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline();
+                    }
+                });
     }
 }
